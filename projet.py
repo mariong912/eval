2f39be92a9130249ea7bb6135e5d18caacf051a0
mariong912 <62334118+mariong912@users.noreply.github.com>
2020-03-26 18:38:49 +0100

    Update projet.py

--- projet.py
+++ projet.py
@@ -4,37 +4,91 @@ Created on Mon Mar 23 15:14:39 2020
 
 @author: Marion
 """
+# A partir de la création d'une fonction qui permet de vréer des polynomes de n'importe quels degrés (simulation)
+# On intégre ces valeurs à un dictionnaires à partir des données (résultats de la fontion) de 2 listes
+# Ensuite on cherche à regarder les 2 séries pour enfin les représenter graphiquements
+"""
+def poly(L,val):
+
+    s=0
+
+    for deg,coef in enumerate(L):
+
+        s=s+ coef*val**deg
+
+    return (s)
+
+rep ="non"
+
+while rep!="oui":
+
+    polynome = []
+
+    degre=int(input("Donne le degré : "))
+
+    for i in range(degre+1):
+
+        polynome.append(float(input("donne le coef ")))
+
+    val_x=float(input("Donnez votre valeur "))
+
+    print(" P(x) = ", poly(polynome, val_x))
+
+    rep=input("voulez-vous continuez ? ")
+
+# On cherche à créer 2 listes des résultats des fonctions polunomiales simulées précédemment
+    
+for i in 5:
+    l1=[]
+    l1.append(poly(polynome, val_x)) 
+
+for j in 5:
+    l2=[]
+    l2.append(poly(polynome, val_x)) 
+# l1 = [57, 23, 101, 819, 242]
+# l2 = [91, 11, 9, 126, 746]
+
+"""
+
+d = {'serie1' : [57, 23, 101, 819, 242],
+     'serie2' : [91, 11, 9, 126, 746]}
 
 import numpy as np 
 import pandas as pd 
 import matplotlib.pyplot as plt
 
-d = {'A' : [14.5, 14., 16., 12.],     
-     'B' : [11., 12., 16., 18.]} 
-# Notes dans une matiere des etudiants A et B
 
 df = pd.DataFrame(d) 
 print(df)
 
-# df['A']+=10
-# df['B']*=2
+# df['serie']+=10
+# df['serie']*=2
 print(df)
 
-serie1=df.mean(1) # calcul de la moyenne par ligne
-serie2=df.mean() # calcul de la moyenne par colonne)
 
-print(serie1)
-print(serie2)
+print("Voici les moyennes par lignes")
+seriemoyl=df.mean(1) # calcul de la moyenne par ligne
+print(seriemoyl)
 
+
+print("Voici les moyennes par série(colonnes)")
+seriemoyc=df.mean() # calcul de la moyenne par colonne)
+print(seriemoyc)
+
+
+print("Voici les sommes cumulees")
 df_cs = df.apply(np.cumsum) # Faire une somme cumulée
 print(df_cs)
 
-x = df['A']; 
-y = df['B'] 
-xm = sum(df['A'])/len(df['A'])
-ym = sum(df['B'])/len(df['B'])
-print(xm,ym)
+x = df['serie1']; 
+y = df['serie2'] 
+xm = sum(df['serie1'])/len(df['serie1'])
+ym = sum(df['serie2'])/len(df['serie2'])
+
+# On cherche à representer graphiquement les 2 series du dictionnaires
 plt.plot(x) 
 plt.plot(y)
-
+# En ajoutant la moyenne de chacune de ces séries qui est calculé au dessus mais également dans la moyenne par serie/colonne
+plt.axhline(xm, color="blue")
+plt.axhline(ym, color="orange")
 plt.show()
